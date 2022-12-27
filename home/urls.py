42e183d543733bab14deca6b71030d4ea2b8aef8
shsxol <shrestha.smile31@gmail.com>
2022-12-27 16:34:35 +0545

    Initial Commit

---
+++ home/urls.py
@@ -0,0 +1,10 @@
+from .views import *
+from django.urls import path
+
+urlpatterns = [
+    path('',HomeView.as_view(), name = 'home'),
+    path('category/<slug>', CategoryView.as_view(), name='category'),
+    path('brand/<slug>', BrandView.as_view(), name='brand'),
+    path('search', SearchView.as_view(), name='search'),
+
+]
---
+++ home/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,redirect
+from django.views.generic import View
+from .models import *
+# Create your views here.
+class BaseView(View):
+    views = {}
+    views['categories'] = Category.objects.all()
+    views['brands'] = Brand.objects.all()
+    views['sale_products'] = Product.objects.filter(label='sale', stock='In stock')
+
+
+class HomeView(BaseView):
+    def get(self,request):
+        self.views
+        self.views['sliders'] = Slider.objects.all()
+        self.views['ads'] = Ad.objects.all()
+        self.views['new_products'] = Product.objects.filter(label = 'new',stock = 'In stock')
+        self.views['hot_products'] = Product.objects.filter(label = 'hot',stock='In stock')
+
+        return render(request,'index.html',self.views)
+
+class CategoryView(BaseView):
+    def get(self,request,slug):
+        ids = Category.objects.get(slug = slug).id
+        cat_name = Category.objects.get(slug = slug).name
+        print(cat_name,ids)
+        self.views['cat_product'] = Product.objects.filter(category_id = ids)
+        print(self.views['cat_product'])
+        return render(request,'category.html',self.views)
+
+class BrandView(BaseView):
+    def get(self, request, slug):
+        ids = Brand.objects.get(slug=slug).id
+        brand_name = Brand.objects.get(slug=slug).name
+        self.views['brand_product'] = Product.objects.filter(brand_id=ids)
+
+
+        return render(request, 'brand.html', self.views)
+
+class SearchView(BaseView):
+    def get(self,request):
+        query = request.GET.get('query')
+        if query != '':
+            self.views['search_product'] = Product.objects.filter(name__icontains = query)
+        else:
+            return redirect('/')
+        return render(request,'search.html',self.views)
